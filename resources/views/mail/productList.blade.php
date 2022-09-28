26bed98eb1a3ad8c479fade3b682ed5b96374df0
Suren <suren.ispiryan2016@gmail.com>
2022-09-28 17:37:47 +0400

    debug

--- resources/views/mail/productList.blade.php
+++ resources/views/mail/productList.blade.php
@@ -1,12 +1,16 @@
-    <h4>New products have been added in last 24 hours. Login to see new product's details.</h4>
-    <p>Here is short list of new added products.</p>
-    @foreach($product as $item)
-        <h5>Name: {{$item->name}}</h5>
-        <h5>Description: {{$item->description}}</h5>
-        <h5>Price: {{$item->price}} $</h5>
+<h4>New products have been added in last 24 hours. Login to see new product's details.</h4>
+<p>Here is short list of new added products.</p>
+@foreach($product as $item)
+    <h5>
+        Name: {{$item->name}}
+        <br>
+        Description: {{$item->description}}
+        <br>
+        Price: {{$item->price}} $
+    </h5>
         <hr>
-    @endforeach
-    @component('mail::button', ['url' => 'http://localhost:3000/login'])
-        Login
-    @endcomponent
-    <h5>Thanks for being member of amazon community.</h5>
+@endforeach
+@component('mail::button', ['url' => 'http://localhost:3000/login'])
+    Login
+@endcomponent
+<h5>Thanks for being member of amazon community.</h5>
